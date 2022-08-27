070b4a014c8ee8a0012be4a1005bb3d4007d8ebf
Afolabi <opakunleafolabi@gmail.com>
2022-08-27 15:26:35 +0100

    code cleaned up

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,10 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostService} from "./post.service";
-import {map} from "rxjs/operators";
 import {IPost} from "./post.interface";
 import {Subscription} from "rxjs";
-import {CategoryService} from "./category.service";
-import {ICategory} from "./category";
 
 @Component({
   selector: 'app-posts',
@@ -12,71 +9,24 @@ import {ICategory} from "./category";
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit, OnDestroy {
-  // body: IPost = {
-  //   categoryId: '-NAALehn-1zRHQP2RkOF',
-  //   title: 'Transformers',
-  //   description: 'Robotic objects rising everywhere'
-  // }
 
   postObserver!: Subscription;
 
   posts!: IPost[];
 
-  constructor(private postService: PostService, private categoryService: CategoryService) { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
     this.getPosts();
-    // this.getCategories();
-    // this.postCategory(this.category);
-    // this.createPost(this.body)
   }
 
   ngOnDestroy(): void {
     this.postObserver.unsubscribe();
   }
 
-  // getPosts() {
-  //   this.postObserver = this.postService.fetchPosts()
-  //     .subscribe(response => {
-  //       this.posts = response;
-  //     })
-  //
-  // }
-
-  createPost(body: IPost) {
-    this.postService.createPost(body)
-      .subscribe(response => {
-        console.log(response)
-      })
-  }
-
-
-  postCategory(category: ICategory) {
-    this.postService.createCategories(category)
-      .subscribe(response => {
-        console.log(response)
-      })
-  }
-
-  getCategories() {
-    this.categoryService.getCategories()
-      .pipe(map(categories => {
-        console.log('CATEGORIES', categories)
-        const categoryData: ICategory[] = []
-        for(let id in categories) {
-          console.log('ID\'S', id)
-          categoryData.push({...categories[id], id})
-        }
-        return categoryData;
-      }))
-      .subscribe(response => {
-        console.log('FINAL' ,response)
-      })
-  }
-
   getPosts() {
     this.postService.getPostsWithCategory()
-      .subscribe(response => {
+      .subscribe((response: IPost[]) => {
         this.posts = response;
       })
   }
